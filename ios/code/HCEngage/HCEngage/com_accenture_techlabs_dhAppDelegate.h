89a9d7cf5900781d5a7b34219f4aa54c7a249402
namelessnerd <karthik.gomadam@gmail.com>
2013-07-24 10:12:34 -0700

    adding a few more files and setting up the ios app

---
+++ ios/code/HCEngage/HCEngage/com_accenture_techlabs_dhAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  com_accenture_techlabs_dhAppDelegate.h
+//  HCEngage
+//
+//  Created by Karthik Gomadam on 7/23/13.
+//  Copyright (c) 2013 Accenture. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class com_accenture_techlabs_dhViewController;
+
+@interface com_accenture_techlabs_dhAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) com_accenture_techlabs_dhViewController *viewController;
+
+@end
---
+++ ios/code/HCEngage/HCEngage/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HCEngage
+//
+//  Created by Karthik Gomadam on 7/23/13.
+//  Copyright (c) 2013 Accenture. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "com_accenture_techlabs_dhAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([com_accenture_techlabs_dhAppDelegate class]));
+    }
+}
