32baacd29cd4ed09c9f7028ad5e177d0d22d8bc2
namelessnerd <karthik.gomadam@gmail.com>
2013-09-10 13:07:33 -0700

    fixed paramter in payload inspection

--- apis/apiapp/routes/personalInfo.js
+++ apis/apiapp/routes/personalInfo.js
@@ -16,28 +16,22 @@ exports.add= function(req, res){
               mongooseHelper.updateDB(consumer,{_id: req.body.consumerID},{$set:{personal_info:req.body.payload.personalInfo}}, 
                                       {upsert:true}, function(err, response){
                                         if (err)
-                                            res.send(responseHelper.errorMSG('Error updating demographic')); 
+                                            res.send(responseHelper.errorMSG('Error updating Personal Info')); 
                                         else
                                             res.send(responseHelper.successMSG('Successfully added Personal Info'));
                                       });
             } // end if demographics keycheck
             else{
-                res.send(responseHelper.errorMSG('Personal Info data missing in payload')); 
+              res.send(responseHelper.errorActionFailed("Add","Personal Info","Missing Personal Info data in payload"));
             }// end else demographics keycheck
         }// end if developer has access to consumer record
         else{
-            res.send(responseHelper.errorMSG('The developer ID you are using does not have permissions'+ 
-                                             'to edit the consumer object with ID' + req.body.consumerID));
+          res.send(responseHelper.errorActionFailed("Add","Personal Info","Missing developer ID, consumer ID or "+
+                                                      "payload attribute"));
         }// end else developer does not have access to consumer record
     
     }// end closure function addPersonalInfo
-  
-  if (req.body.developerID && req.body.consumerID){
-    console.log(" Both IDs are present ");
-    devIDChecker.check(req.body.developerID, req.body.consumerID, addPersonalInfo);
-  }
-  else
-    res.send(responseHelper.errorMSG('Adding Personal Info requires a consumer ID and a developerID'));
+  devIDChecker.check(req,  addPersonalInfo);
 }
 
 exports.update= function(req, res){
@@ -64,20 +58,14 @@ exports.update= function(req, res){
           });
       } // end if attributes keycheck
       else{
-        res.send(responseHelper.errorMSG('No attributes to update. Please refer to update documentation.')); 
+        res.send(responseHelper.errorActionFailed("Update","Personal Info","Missing attribute data in payload"));
       }// end else attributes keycheck
     }// end if developer has access to consumer record
     else{
       console.log('id server responded with an error');
-      res.send(responseHelper.errorMSG('The developer ID you are using does not have permissions'+ 
-                                       'to edit the consumer object with ID' + req.body.consumerID));
+      res.send(responseHelper.errorActionFailed("Update","Personal Info","Missing developer ID, consumer ID or "+
+                                                      "payload attribute"));
     }// end else developer does not have access to consumer record
   }// end closure function addPersonalInfo
-  
-  if (req.body.developerID && req.body.consumerID){
-    console.log(" Both IDs are present ");
-    devIDChecker.check(req.body.developerID, req.body.consumerID, updatePersonalInfo);
-  }
-  else
-    res.send(responseHelper.errorMSG('Updating Personal Info requires a consumer ID and a developerID'));
+  devIDChecker.check(req,  addPersonalInfo);
 }
